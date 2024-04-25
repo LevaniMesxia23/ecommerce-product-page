7539c9e7177e2e1b358762637552ab82bedb12c1
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-04-25 23:32:40 +0400

    add: Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
+import Navigation from "../src/components/Navigation"
+import GlobalStyles from "./styles/GlobalStyles"
+import styled from "styled-components"
 
 function App() {
 
-
   return (
     <>
-      
+      <GlobalStyles />
+      <Navigation />
     </>
   )
 }
 
 export default App
+
