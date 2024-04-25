aeb268b00264f9e9584c624bf4d639d609a37ef9
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-04-25 23:49:23 +0400

    add: GlobalStyles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Kumbh+Sans:wght@100..900&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Kumbh Sans", sans-serif;
+}
+`;
 
-`
-
-export default GlobalStyles
+export default GlobalStyles;
