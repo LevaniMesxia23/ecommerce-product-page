07579921b67fc6d39abfb2e9cf33d0477e4c3665
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-04-27 11:46:46 +0400

    cleared code

--- src/components/MainImages.jsx
+++ src/components/MainImages.jsx
@@ -1,44 +1,3 @@
-// import React from "react";
-// import styled from "styled-components";
-// import Product1 from "../../public/images/image-product-1.jpg";
-
-// export default function MainImages() {
-//   return (
-//     <>
-//       <Container>
-//         <div>
-//           <img src={Product1} alt="" />
-
-          
-//         </div>
-//       </Container>
-//     </>
-//   );
-// }
-
-// const Container = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   width: 100%;
-
-//   div {
-//     position: relative;
-
-//     img {
-//       width: 100%;
-//       min-width: 21.4375rem;
-//       height: 18.75rem;
-//       flex-shrink: 0;
-//     }
-    
-//   }
-// `;
-
-
-
-
-
-
 import Product1 from "../../public/images/image-product-1.jpg";
 import Product2 from "../../public/images/image-product-2.jpg";
 import Product3 from "../../public/images/image-product-3.jpg";
@@ -80,38 +39,6 @@ export default function App() {
         <SwiperSlide>
         <img src={Product4} alt="Product4" />
         </SwiperSlide>
-
-        {/* <svg
-            className="left"
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            viewBox="0 0 40 40"
-            fill="none"
-          >
-            <circle cx="20" cy="20" r="20" fill="white" />
-            <path
-              d="M22.8572 14.2857L17.1429 20L22.8572 25.7143"
-              stroke="#1D2026"
-              stroke-width="3"
-            />
-          </svg>
-
-          <svg
-            className="rigth"
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            viewBox="0 0 40 40"
-            fill="none"
-          >
-            <circle cx="20" cy="20" r="20" fill="white" />
-            <path
-              d="M17.1428 14.2857L22.8571 20L17.1428 25.7143"
-              stroke="#1D2026"
-              stroke-width="3"
-            />
-          </svg> */}
       </Swiper>
     </>
   );
